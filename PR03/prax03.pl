fa1140f2152cbf62d103b19942533b2f813398a7
karlviik <viik.karl2@gmail.com>
2019-09-23 17:02:57 +0300

    Ül2

--- PR03/prax03.pl
+++ PR03/prax03.pl
@@ -2,4 +2,17 @@
 % 1. Kirjutada rekursiivne reegel viimane_element/2, mis leiab listi viimase elemendi.
 viimane_element(_, []) :- fail.
 viimane_element(E, [E | []]).
-viimane_element(E, [_ | T]).
+viimane_element(E, [_ | T]) :- viimane_element(E, T).
+
+
+% 2. Kirjutada reegel suurim/2, mis kontrollib etteantud listist järjest paarikaupa elemente
+% ja paneb väljundlisti elemendi, mis on antud paari elementidest suurim.
+% Kui võrreldakse elementi ja tühilisti,siis väljundlisti tuleb panna element.
+suurim([X | []], Y) :- append([], [X], Y).
+suurim([H1, H2 | []], X) :- Bigger is max(H1, H2),
+                            suurim([H2], Xsmaller),
+                            append(Bigger, Xsmaller, X).
+suurim([H1, H2 | T], X) :- Bigger is max(H1, H2),
+                            append([H2], T, Tsmaller),
+                            suurim(Tsmaller, Xsmaller),
+                            append([Bigger], Xsmaller, X).
