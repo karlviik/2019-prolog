818a00e5755d3d5ffb220dabac869355d77c5554
karlviik <viik.karl2@gmail.com>
2019-09-23 16:16:24 +0300

    Ül1 fix

--- PR03/prax03.pl
+++ PR03/prax03.pl
@@ -1,5 +1,5 @@
 
 % 1. Kirjutada rekursiivne reegel viimane_element/2, mis leiab listi viimase elemendi.
-viimane_element(_, []).
+viimane_element(_, []) :- fail.
 viimane_element(E, [E | []]).
 viimane_element(E, [_ | T]).
