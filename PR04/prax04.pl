199f67ef43ddc0afbb8f2c3d245b79c1db832a97
karlviik <viik.karl2@gmail.com>
2019-10-03 10:46:54 +0300

    Backup

--- PR04/prax04.pl
+++ PR04/prax04.pl
@@ -18,10 +18,30 @@ lennukiga(paris, tallinn, 120).
 %lennukiga(From, Where, Time) :- lennukiga(From, Where, Time) ; lennukiga(Where, From, Time).
 
 
+mine(From, Where) :- (laevaga(From, Where, _) ; laevaga(Where, From, _) ;
+                    bussiga(From, Where, _) ; bussiga(Where, From, _) ;
+                    rongiga(From, Where, _) ; rongiga(Where, From, _) ;
+                    lennukiga(From, Where, _) ; lennukiga(Where, From, _)), asserta(labitud(From)).
 
-reisi(From, Where) :- (laevaga(From, Where, _) ; laevaga(Where, From, _) ;
+mine(From, Next, Path) :- not(mine(From, Next)), Path = mine(Next, Nextynext, Smallerpath).
+
+reisi_a(From, Where) :- (laevaga(From, Where, _) ; laevaga(Where, From, _) ;
                         bussiga(From, Where, _) ; bussiga(Where, From, _) ;
                         rongiga(From, Where, _) ; rongiga(Where, From, _) ;
                         lennukiga(From, Where, _) ; lennukiga(Where, From, _)) , asserta(labitud(From)).
 
-reisi(From, Where) :- not(labitud(From)), reisi(From, Somewhere), reisi(Somewhere, Where).
+reisi_a(From, Where) :- not(labitud(From)), reisi_a(From, Somewhere), reisi_a(Somewhere, Where).
+
+reisi_a(From, Where, mine(From, Where)) :- (laevaga(From, Where, _) ; laevaga(Where, From, _) ;
+                                           bussiga(From, Where, _) ; bussiga(Where, From, _) ;
+                                           rongiga(From, Where, _) ; rongiga(Where, From, _) ;
+                                           lennukiga(From, Where, _) ; lennukiga(Where, From, _)), asserta(labitud(From)).
+
+reisi_a(From, Where, mine(From, Next, Path)) :- reisi(From, Where), asserta(labitud(From)).
+
+reisi(From, Where) :- retractall(labitud(_)), reisi_a(From, Where), retractall(labitud(_)) .
+
+
+
+
+
