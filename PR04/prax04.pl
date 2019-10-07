41368de5f28b9d0083abf82df71130f6010ea7d5
karlviik <viik.karl2@gmail.com>
2019-10-07 11:18:22 +0300

    Switching computers

--- PR04/prax04.pl
+++ PR04/prax04.pl
@@ -1,4 +1,5 @@
 :- dynamic labitud/1.
+:- dynamic goal/1.
 
 laevaga(tallinn, helsinki, 120).
 laevaga(tallinn, stockholm, 480).
@@ -21,26 +22,29 @@ lennukiga(paris, tallinn, 120).
 mine(From, Where) :- (laevaga(From, Where, _) ; laevaga(Where, From, _) ;
                     bussiga(From, Where, _) ; bussiga(Where, From, _) ;
                     rongiga(From, Where, _) ; rongiga(Where, From, _) ;
-                    lennukiga(From, Where, _) ; lennukiga(Where, From, _)), asserta(labitud(From)).
+                    lennukiga(From, Where, _) ; lennukiga(Where, From, _)), goal(Where).
 
-mine(From, Next, Path) :- not(mine(From, Next)), Path = mine(Next, Nextynext, Smallerpath).
+mine(From, Next, Path) :- not(mine(From, Next)), labitud(Nextynext), retractall(labitud(Nextynext)), (goal(Nextynext), Path = mine(Next, Nextynext) ; Path = mine(Next, Nextynext, Sub)).
 
 reisi_a(From, Where) :- (laevaga(From, Where, _) ; laevaga(Where, From, _) ;
                         bussiga(From, Where, _) ; bussiga(Where, From, _) ;
                         rongiga(From, Where, _) ; rongiga(Where, From, _) ;
                         lennukiga(From, Where, _) ; lennukiga(Where, From, _)) , asserta(labitud(From)).
 
-reisi_a(From, Where) :- not(labitud(From)), reisi_a(From, Somewhere), reisi_a(Somewhere, Where).
+reisi_a(From, Where) :- not(labitud(From)), reisi_a(From, Somewhere), write(Somewhere), reisi_a(Somewhere, Where).
 
-reisi_a(From, Where, mine(From, Where)) :- (laevaga(From, Where, _) ; laevaga(Where, From, _) ;
-                                           bussiga(From, Where, _) ; bussiga(Where, From, _) ;
-                                           rongiga(From, Where, _) ; rongiga(Where, From, _) ;
-                                           lennukiga(From, Where, _) ; lennukiga(Where, From, _)), asserta(labitud(From)).
 
-reisi_a(From, Where, mine(From, Next, Path)) :- reisi(From, Where), asserta(labitud(From)).
+reisi_a(From, Where, Path) :- retractall(labitud(_)), reisi_a(From, Where), asserta(goal(Where)),
+                            (Path = mine(From, Where) ; labitud(Next), Path = mine(From, Next, Sub)).
 
 reisi(From, Where) :- retractall(labitud(_)), reisi_a(From, Where), retractall(labitud(_)) .
 
+%reisi_a(From, Where, mine(From, Where)) :- (laevaga(From, Where, _) ; laevaga(Where, From, _) ;
+%                                           bussiga(From, Where, _) ; bussiga(Where, From, _) ;
+%                                           rongiga(From, Where, _) ; rongiga(Where, From, _) ;
+%                                           lennukiga(From, Where, _) ; lennukiga(Where, From, _)), asserta(labitud(From)).
+
+%reisi_a(From, Where, mine(From, Next, Path)) :- reisi(From, Where), asserta(labitud(From)).
 
 
 
