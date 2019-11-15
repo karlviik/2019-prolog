3b2d3f3a0ba564b8b5681507a4a7d706cb8eb5aa
karlviik <viik.karl2@gmail.com>
2019-11-16 00:11:39 +0200

    Test

--- PR10/prax10.pl
+++ PR10/prax10.pl
@@ -84,40 +84,40 @@ kas_naaber_vaba(X,Y,X1,Y1):-
 
 %---------MÄNGU ALGSEIS-------------
 % Valged
-ruut(1,1,1).
-ruut(1,3,1).
-ruut(1,5,1).
-ruut(1,7,1).
-ruut(2,2,1).
-ruut(2,4,1).
-ruut(2,6,1).
-ruut(2,8,1).
-ruut(3,1,1).
-ruut(3,3,1).
-ruut(3,5,1).
-ruut(3,7,1).
-% Tühjad ruudud
-ruut(4,2,0).
-ruut(4,4,0).
-ruut(4,6,0).
-ruut(4,8,0).
-ruut(5,1,0).
-ruut(5,3,0).
-ruut(5,5,0).
-ruut(5,7,0).
-% Mustad
-ruut(6,2,2).
-ruut(6,4,2).
-ruut(6,6,2).
-ruut(6,8,2).
-ruut(7,1,2).
-ruut(7,3,2).
-ruut(7,5,2).
-ruut(7,7,2).
-ruut(8,2,2).
-ruut(8,4,2).
-ruut(8,6,2).
-ruut(8,8,2).
+%ruut(1,1,1).
+%ruut(1,3,1).
+%ruut(1,5,1).
+%ruut(1,7,1).
+%ruut(2,2,1).
+%ruut(2,4,1).
+%ruut(2,6,1).
+%ruut(2,8,1).
+%ruut(3,1,1).
+%ruut(3,3,1).
+%ruut(3,5,1).
+%ruut(3,7,1).
+%% Tühjad ruudud
+%ruut(4,2,0).
+%ruut(4,4,0).
+%ruut(4,6,0).
+%ruut(4,8,0).
+%ruut(5,1,0).
+%ruut(5,3,0).
+%ruut(5,5,0).
+%ruut(5,7,0).
+%% Mustad
+%ruut(6,2,2).
+%ruut(6,4,2).
+%ruut(6,6,2).
+%ruut(6,8,2).
+%ruut(7,1,2).
+%ruut(7,3,2).
+%ruut(7,5,2).
+%ruut(7,7,2).
+%ruut(8,2,2).
+%ruut(8,4,2).
+%ruut(8,6,2).
+%ruut(8,8,2).
 
 /*
 ruut(X,Y, Status).  %   kus X, Y [1,8]
